a285fe32e577f8febf12d47431d68ecb19156fb0
X@nxus <47295820+Xanxus-5@users.noreply.github.com>
2019-05-04 12:31:15 +0800

    Add files via upload

---
+++ CCEncode.py
@@ -0,0 +1,10 @@
+ptxt = input("Please type the words:")
+for p in ptxt:
+    if "a" <= p <= "z":
+        print(chr(ord("a") + (ord(p) - ord("a") + 3) % 26), end='')
+    elif "A" <= p <= "Z":
+        print(chr(ord("A") + (ord(p) - ord("A") + 3) % 26), end='')
+    elif 0x4E00 <= ord(p) <= 0x9FA5 :
+        print(chr(ord(p) + 3), end='')
+    else:
+        print(p, end='')
